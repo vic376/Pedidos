a08215ff9424d9d46a83595895f1a199be3fffe6
vic376 <nicoladellivick@gmail.com>
2026-03-20 15:39:23 -0300

    Finalizacao do banco-pt2

--- Pedidos/src/main/java/com/senai/weg/Pedidos/controller/ItemPedidoController.java
+++
@@ -1,4 +0,0 @@
-package com.senai.weg.Pedidos.controller;
-
-public class ItemPedidoController {
-}
--- Pedidos/src/main/java/com/senai/weg/Pedidos/dto/request/ItemPedidoRequest.java
+++
@@ -1,4 +0,0 @@
-package com.senai.weg.Pedidos.dto.request;
-
-public class ItemPedidoRequest {
-}
